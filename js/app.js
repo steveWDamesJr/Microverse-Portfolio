a8c2fa18dc967a76a5c406e67f9043fba7596842
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2021-12-21 14:41:12 -0500

    Update markup and stylesheet
    -Debug linters
    -Reassign section on Portfolio link click to works section

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const bar2 = document.querySelector(".bar2")
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-})
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
