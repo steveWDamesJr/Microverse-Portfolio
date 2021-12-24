89496969918e716288fd8397069d13a67c979b71
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2021-12-24 14:23:34 -0500

    Fix linter errors

--- js/app.js
+++ js/app.js
@@ -18,118 +18,126 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
 const workObject = [{
   id: 1,
   workCardTitle: 'Tonic',
-  imagesMobile: './images/popup-tonic-mobile.png',
-  imagesDesk: './images/desk-snap-4.png',
+  imagesMobile: ['./images/popup-tonic-mobile.png', './images/mps1.png'],
+  imagesDesk: ['./images/desk-snap-4.png', './images/desk-snap-1.png'],
   imageAlt: 'Tonic Project',
-  cardTitleMo: 'Tonic',
-  cardTitleDesk: 'Tonic',
+  cardTitleMo: ['Tonic', 'Multi-Post Stories'],
+  cardTitleDesk: ['Tonic', 'Multi-Post Stories'],
   titleElementsMo: ['CANOPY', 'Back End Dev', '2015'],
-  titleElementsDesk: ['CANOPY', 'Back End Dev', '2015'],
+  titleElementsDesk: ['CANOPY', 'Back End Dev', '2015', 'FACEBOOK', 'Full Stack Dev', '2015'],
   cardDescriptionMo: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  cardDescriptionDesk: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  cardDescriptionDesk: ['A daily selection of privately personalized reads; no accounts or sign-ups required.', 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.'],
   languagesMo: ['html', 'css', 'javaScript'],
-  languagesDesk: ['html', 'css', 'javaScript'],
+  languagesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
   liveBtn: '#',
-  sourceBtn: '#'
+  sourceBtn: '#',
 },
 {
   id: 2,
   workCardTitle: 'Multi-Post Stories',
-  imagesMobile: './images/mps1.png',
-  imagesDesk: './images/desk-snap-1.png',
+  imagesMobile: ['./images/Tonic2.png', './images/mps2.png'],
+  imagesDesk: ['./images/desk-snap-2.png', './images/desk-snap-3.png'],
   imageAlt: 'Multi-Post Project 1',
-  cardTitleMo: 'Multi-Post Stories',
-  cardTitleDesk: 'Multi-Post Stories',
+  cardTitleMo: ['Tonic', 'Multi-Post Stories'],
+  cardTitleDesk: ['Facebook 360', 'Uber Navigation'],
   titleElementsMo: ['CANOPY', 'Back End Dev', '2015'],
-  titleElementsDesk: ['FACEBOOK', 'Full Stack Dev', '2015'],
+  titleElementsDesk: ['FACEBOOK', 'Full Stack Dev', '2015', 'UBER', 'Lead Developer', '2018'],
   cardDescriptionMo: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  cardDescriptionDesk: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+  cardDescriptionDesk: ['Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.', 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.'],
   languagesMo: ['html', 'css', 'javaScript'],
   languagesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
   liveBtn: '#',
-  sourceBtn: '#'
-},
-{
-  id: 3,
-  workCardTitle: 'Tonic',
-  imagesMobile: './images/Tonic2.png',
-  imagesDesk: './images/desk-snap-2.png',
-  imageAlt: 'Tonic Project 2',
-  cardTitleMo: 'Tonic',
-  cardTitleDesk: 'Facebook 360',
-  titleElementsMo: ['CANOPY', 'Back End Dev', '2015'],
-  titleElementsDesk: ['FACEBOOK', 'Full Stack Dev', '2015'],
-  cardDescriptionMo: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  cardDescriptionDesk: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-  languagesMo: ['html', 'css', 'javaScript'],
-  languagesDesk: ['html', 'Ruby on rails', 'css', 'javaScript'],
-  liveBtn: '#',
-  sourceBtn: '#'
-},
-{
-  id: 4,
-  workCardTitle: 'Multi-Post Stories',
-  imagesMobile: './images/mps2.png',
-  imagesDesk: './images/desk-snap-3.png',
-  imageAlt: 'Multi-Post Project 2',
-  cardTitleMo: 'Multi-Post Stories',
-  cardTitleDesk: 'Uber Navigation',
-  titleElementsMo: ['CANOPY', 'Back End Dev', '2015'],
-  titleElementsDesk: ['UBER', 'Lead Developer', '2018'],
-  cardDescriptionMo: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-  cardDescriptionDesk: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-  languagesMo: ['html', 'css', 'javaScript'],
-  languagesDesk: ['html', 'css', 'javaScript'],
-  liveBtn: '#',
-  sourceBtn: '#'
+  sourceBtn: '#',
 },
 ];
 
-for (let i = 0; i < workObject.length; i += 1) {
-  const allWork = workObject[i];
-  const section = document.createElement('section');
-  section.setAttribute('class', 'card');
+for (let i = 0; i < 1; i += 1) {
+  for (let j = 0; j < workObject.length; j += 1) {
+    const allWork = workObject[j];
+    const section = document.createElement('section');
+    section.setAttribute('class', 'card');
 
-  worksSection.innerHTML += `
+    worksSection.innerHTML += `
   
   <section class="card">
-  <img class="desk-card-img" src="${allWork.imagesDesk}" alt="Desktop Tonic Project">
-  <img class="card-img" src="${allWork.imagesMobile}" alt="Tonic Project 1">
+  <img class="desk-card-img" src=${allWork.imagesDesk[i]} alt="Desktop Tonic Project">
+  <img class="card-img" src="${allWork.imagesMobile[i]}" alt="Tonic Project 1">
   <div class="text-cont">
-    <h2 class="card-title">${allWork.cardTitleMo}</h2>
-    <h2 class="card-desk-title">${allWork.cardTitleDesk}</h2>
+    <h2 class="card-title">${allWork.cardTitleMo[i]}</h2>
+    <h2 class="card-desk-title">${allWork.cardTitleDesk[i]}</h2>
     <ul class="title-elements">
       <li>
         <p class="pr-name">${allWork.titleElementsMo[0]}</p>
-        <p class="pr-desk-name">${allWork.titleElementsDesk[0]}</p>
+        <p class="pr-desk-name">${allWork.titleElementsDesk[i]}</p>
       </li>
       <li><img class="dot" src="./images/dot.png" alt="stylistic text divider dot"></li>
       <li>
         <p class="art-name">${allWork.titleElementsMo[1]}</p>
-        <p class="art-desk-name">${allWork.titleElementsDesk[1]}</p>
+        <p class="art-desk-name">${allWork.titleElementsDesk[i + 1]}</p>
       </li>
       <li><img class="dot" src="./images/dot.png" alt="stylistic text divider dot"></li>
       <li>
         <p class="year">${allWork.titleElementsMo[2]}</p>
-        <p class="desk-year">${allWork.titleElementsDesk[2]}</p>
+        <p class="desk-year">${allWork.titleElementsDesk[i + 2]}</p>
       </li>
     </ul>
 
-    <p class="card-description">A daily selection of privately personalized reads; no accounts or sign-ups
-      required.</p>
-    <p class="card-description desk-card-des">A daily selection of privately personalized reads; no accounts or
-      sign-ups required.</p>
+    <p class="card-description">${allWork.cardDescriptionMo}</p>
+    <p class="card-description desk-card-des">${allWork.cardDescriptionDesk[i]}</p>
+  
     <ul class="card-subtitles card-desk-sub">
-      <li class="languages">html</li>
-      <li class="languages">css</li>
-      <li class="languages">javaScript</li>
+      <li class="languages">${allWork.languagesDesk[i]}</li>
+      <li class="languages">${allWork.languagesDesk[i + 2]}</li>
+      <li class="languages">${allWork.languagesDesk[i + 3]}</li>
+    
+      
+      
     </ul>
 
     <a class="card-button data-popup-ref" href="#">See Project</a>
   </div>
 </section>
 
+<section class="card">
+  
+  <img class="card-img" src="${allWork.imagesMobile[i + 1]}" alt="Tonic Project 1">
+  <div class="text-cont">
+    <h2 class="card-title">${allWork.cardTitleMo[i + 1]}</h2>
+    <h2 class="card-desk-title">${allWork.cardTitleDesk[i + 1]}</h2>
+    <ul class="title-elements">
+      <li>
+        <p class="pr-name">${allWork.titleElementsMo[0]}</p>
+        <p class="pr-desk-name">${allWork.titleElementsDesk[i + 3]}</p>
+      </li>
+      <li><img class="dot" src="./images/dot.png" alt="stylistic text divider dot"></li>
+      <li>
+        <p class="art-name">${allWork.titleElementsMo[1]}</p>
+        <p class="art-desk-name">${allWork.titleElementsDesk[i + 4]}</p>
+      </li>
+      <li><img class="dot" src="./images/dot.png" alt="stylistic text divider dot"></li>
+      <li>
+        <p class="year">${allWork.titleElementsMo[2]}</p>
+        <p class="desk-year">${allWork.titleElementsDesk[i + 5]}</p>
+      </li>
+    </ul>
+
+    <p class="card-description">${allWork.cardDescriptionMo}</p>
+    <p class="card-description desk-card-des">${allWork.cardDescriptionDesk[i + 1]}</p>
+    <ul class="card-subtitles card-desk-sub lang-width">
+    <li class="languages">${allWork.languagesDesk[i]}</li>
+    <li class="languages hideMe">${allWork.languagesDesk[i + 1]}</li>
+    <li class="languages">${allWork.languagesDesk[i + 2]}</li>
+    <li class="languages">${allWork.languagesDesk[i + 3]}</li>
+    
+  </ul>
+
+    <a class="card-button data-popup-ref" href="#">See Project</a>
+  </div>
+  <img class="desk-card-img" src="${allWork.imagesDesk[i + 1]}" alt="Desktop Tonic Project">
+</section>
   `;
+  }
+}
 
 const appbtn = document.querySelectorAll('.data-popup-ref');
 const wrapper = document.querySelector('.wrapper');
@@ -159,4 +167,5 @@ eForm.addEventListener('submit', (e) => {
   }else {e.preventDefault(); }
   emailMsg.innerHTML = '*Please enter correct email in lower case';
   emailMsg.style.color = 'red';
-})};
+})
+
